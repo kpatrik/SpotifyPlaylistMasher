9fe5d4e3a58d8d8a4a6db827f42e2c66fbc4aa0d
Patrik Koleszár <patrik5000@gmail.com>
2018-09-29 12:24:43 +0200

    feat(index): allow cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers","*")
+
+  next();
+}
+
+app.use(allowCrossDomain);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
